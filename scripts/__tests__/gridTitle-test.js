f07122d1e1411d6290f2da6cb1de62d807e3972b
Liang Huang <Liang_Huang@intuit.com>
2015-05-14 15:36:55 -0600

    Added unit tests for column groups

--- scripts/__tests__/gridTitle-test.js
+++ scripts/__tests__/gridTitle-test.js
@@ -5,7 +5,8 @@ jest.dontMock('../columnProperties.js');
 var React = require('react/addons');
 var GridTitle = require('../gridTitle.jsx');
 var TestUtils = React.addons.TestUtils;
-var ColumnProperties = require('../columnProperties.js'); 
+var ColumnProperties = require('../columnProperties.js');
+var _ = require('underscore');
 
 describe('GridTitle', function() {
 	var title; 
@@ -109,5 +110,118 @@ describe('GridTitle', function() {
 		React.addons.TestUtils.Simulate.click(second, otherEvent);	
 		expect(mock.mock.calls.length).toEqual(1);
 		expect(mock.mock.calls[0]).toEqual({0:"two"});
+	});
+
+
+	var getColumnSettings = function(columns, columnMetaData) {
+		return new ColumnProperties(columns, [], "children", columnMetaData, [])
+	};
+	var getRenderedTitle = function(columns, columnMetaData) {
+		return TestUtils.renderIntoDocument(<GridTitle columns={columns} columnSettings={getColumnSettings(columns, columnMetaData)} sortSettings={sortObject}/>);
+	};
+	var getColumns = function(columnMetaData) {
+		return _.pluck(columnMetaData, "columnName");
+	};
+	var getHeaders = function(title) {
+		return TestUtils.scryRenderedDOMComponentsWithTag(title, 'tr');
+	};
+	var getColumnGroupColSpans = function(columnGroups, columnMetaData) {
+		return _.map(columnGroups, function(groupName){
+			return _.filter(columnMetaData, function(columnMeta){
+				return columnMeta.columnGroupName === groupName;
+			}).length;
+		});
+	};
+
+	it('should display column groups when configured', function() {
+		var columnMetaData = [
+			{
+				columnName: "id",
+				order: 1,
+				visible: true,
+				columnGroupName: "Name"
+			},
+			{
+				columnName: "name",
+				order: 2,
+				visible: true,
+				columnGroupName: "Name"
+			},
+			{
+				columnName: "city",
+				order: 3,
+				visible: true,
+				columnGroupName: "Address"
+			},
+			{
+				columnName: "state",
+				order: 4,
+				visible: true,
+				columnGroupName: "Address"
+			}
+		];
+		var columns = getColumns(columnMetaData);
+		var headers = getHeaders(getRenderedTitle(columns, columnMetaData));
+		var columnGroups = _.uniq(_.pluck(columnMetaData, "columnGroupName"));
+		var columnGroupColSpans = getColumnGroupColSpans(columnGroups, columnMetaData);
+
+		var columnGroupHeaders = TestUtils.scryRenderedDOMComponentsWithTag(headers[0], 'th');
+		expect(columnGroupHeaders.length).toEqual(columnGroups.length);
+
+		TestUtils.scryRenderedDOMComponentsWithTag(headers[0], 'th').forEach(function(columnGroup, index){
+			expect(columnGroup.props.children).toEqual(columnGroups[index]);
+			expect(columnGroup.props.colSpan).toEqual(columnGroupColSpans[index]);
+		});
+		TestUtils.scryRenderedDOMComponentsWithTag(headers[1], 'th').forEach(function(column, index){
+			expect(column.props.children[0]).toEqual(columns[index]);
+		});
+	});
+
+	it('should display column names as column group names when column group names are not specified', function() {
+		var columnMetaData = [
+			{
+				columnName: "id",
+				order: 1,
+				visible: true
+			},
+			{
+				columnName: "name",
+				order: 2,
+				visible: true
+			},
+			{
+				columnName: "city",
+				order: 3,
+				visible: true,
+				columnGroupName: "Address"
+			},
+			{
+				columnName: "state",
+				order: 4,
+				visible: true,
+				columnGroupName: "Address"
+			}
+		];
+		var columns = getColumns(columnMetaData);
+		var headers = getHeaders(getRenderedTitle(columns, columnMetaData));
+		var columnGroups = _.uniq(_.map(_.pluck(columnMetaData, "columnGroupName"), function(groupName, index){
+			return groupName || columnMetaData[index].columnName;
+		}));
+		var columnGroupColSpans = getColumnGroupColSpans(columnGroups, columnMetaData);
+
+		var columnGroupHeaders = TestUtils.scryRenderedDOMComponentsWithTag(headers[0], 'th');
+		expect(columnGroupHeaders.length).toEqual(columnGroups.length);
+
+		columnGroupHeaders.forEach(function(columnGroup, index){
+			expect(columnGroup.props["data-title"] || columnGroup.props.children).toEqual(columnGroups[index]);
+			if (columnGroupColSpans[index]) {
+				expect(columnGroup.props.colSpan).toEqual(columnGroupColSpans[index]);
+			}
+		});
+		TestUtils.scryRenderedDOMComponentsWithTag(headers[1], 'th').forEach(function(column, index){
+			if (columnGroupColSpans[index]) {
+				expect(column.props.children[0]).toEqual(columns[index]);
+			}
+		});
 	});
 });
