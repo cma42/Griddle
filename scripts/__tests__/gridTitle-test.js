f07aa3fd0b9c786f34f01bdb866c497d2660e0d4
Liang Huang <Liang_Huang@intuit.com>
2015-05-14 15:41:02 -0600

    Refactored the unit tests for grid title

--- scripts/__tests__/gridTitle-test.js
+++ scripts/__tests__/gridTitle-test.js
@@ -9,32 +9,28 @@ var ColumnProperties = require('../columnProperties.js');
 var _ = require('underscore');
 
 describe('GridTitle', function() {
-	var title; 
-	var columns; 
-	var columnSettings; 
-	var sortObject; 
+	var getColumnSettings = function(columns, columnMetaData) {
+		return new ColumnProperties(columns, [], "children", columnMetaData, [])
+	};
+	var getRenderedTitle = function(columns, columnMetaData) {
+		var sortObject = {
+			enableSort: true,
+			changeSort: null,
+			sortColumn: "",
+			sortAscending: true,
+			sortAscendingClassName: "",
+			sortDescendingClassName: "",
+			sortAscendingComponent: null,
+			sortDescendingComponent: null
+		};
+		return TestUtils.renderIntoDocument(<GridTitle columns={columns} columnSettings={getColumnSettings(columns, columnMetaData)} sortSettings={sortObject}/>);
+	};
 
-	beforeEach(function(){
-		columns = ["one", "two", "three"];
-		columnSettings = new ColumnProperties(columns, [], "children", [], []);
-    sortObject =  { 
-        enableSort: true,
-        changeSort: null, 
-        sortColumn: "",
-        sortAscending: true, 
-        sortAscendingClassName: "", 
-        sortDescendingClassName: "",
-        sortAscendingComponent: null,
-        sortDescendingComponent: null
-    }
+	it('calls method when clicked', function() {
+		var title = getRenderedTitle(["one", "two", "three"], []);
 
-    title = TestUtils.renderIntoDocument(<GridTitle columns={columns} columnSettings={columnSettings} sortSettings={sortObject}/>);
-	});
-
-	it('calls method when clicked', function(){
 		var node = TestUtils.findRenderedDOMComponentWithTag(title, 'thead');
 		var headings = TestUtils.scryRenderedDOMComponentsWithTag(node, 'th');
-
 		var mock = jest.genMockFunction(); 
 		title.props.sortSettings.changeSort = mock;
 
@@ -54,29 +50,26 @@ describe('GridTitle', function() {
 		};
 		React.addons.TestUtils.Simulate.click(first, someEvent);
 
-    expect(mock.mock.calls.length).toEqual(1);
+    	expect(mock.mock.calls.length).toEqual(1);
 		expect(mock.mock.calls[0]).toEqual({0: 'one'});
-
 	});
 
-	it('doesnt sort column where sort is disabled', function(){
-		var newMeta = [{
-	    "columnName": "one",
-	    "order": 2,
-	    "locked": false,
-	    "visible": true,
-	    "displayName": "Name",
-	    "sortable" : false
-	  }];
-		var columnSettings2 = new ColumnProperties(columns, [], "children", newMeta, []);
+	it('doesnt sort column where sort is disabled', function() {
+		var columnMetaData = [{
+			"columnName": "one",
+			"order": 2,
+			"locked": false,
+			"visible": true,
+			"displayName": "Name",
+			"sortable" : false
+		}];
+		var title = getRenderedTitle(["one", "two", "three"], columnMetaData);
 
-		var title2 = TestUtils.renderIntoDocument(<GridTitle columns={columns} columnSettings={columnSettings2} sortSettings={sortObject}/>);
-
-		var node = TestUtils.findRenderedDOMComponentWithTag(title2, 'thead');
+		var node = TestUtils.findRenderedDOMComponentWithTag(title, 'thead');
 		var headings = TestUtils.scryRenderedDOMComponentsWithTag(node, 'th');
 
 		var mock = jest.genMockFunction(); 
-		title2.props.sortSettings.changeSort = mock;
+		title.props.sortSettings.changeSort = mock;
 
 		expect(headings.length).toEqual(3);
 
@@ -85,7 +78,7 @@ describe('GridTitle', function() {
 
 		expect(TestUtils.isDOMComponent(first)).toBe(true);
 		expect(TestUtils.isDOMComponent(second)).toBe(true);
-		expect(title2.props.sortSettings.sortColumn).toEqual("");
+		expect(title.props.sortSettings.sortColumn).toEqual("");
 
 		//todo: can we just get this from jsdom?
 		var someEvent = {
@@ -105,20 +98,13 @@ describe('GridTitle', function() {
 		};
 		React.addons.TestUtils.Simulate.click(first, someEvent);
 
-    expect(mock.mock.calls.length).toEqual(0);
+    	expect(mock.mock.calls.length).toEqual(0);
 
 		React.addons.TestUtils.Simulate.click(second, otherEvent);	
 		expect(mock.mock.calls.length).toEqual(1);
 		expect(mock.mock.calls[0]).toEqual({0:"two"});
 	});
 
-
-	var getColumnSettings = function(columns, columnMetaData) {
-		return new ColumnProperties(columns, [], "children", columnMetaData, [])
-	};
-	var getRenderedTitle = function(columns, columnMetaData) {
-		return TestUtils.renderIntoDocument(<GridTitle columns={columns} columnSettings={getColumnSettings(columns, columnMetaData)} sortSettings={sortObject}/>);
-	};
 	var getColumns = function(columnMetaData) {
 		return _.pluck(columnMetaData, "columnName");
 	};
